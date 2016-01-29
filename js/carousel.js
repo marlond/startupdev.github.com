6854fe647d0dc916a6b7c02733814c25d8d6a6a1
cacauinbox <cacauinbox@gmail.com>
2016-01-29 09:40:28 -0200

    Carousel sizing and multiplicity adjustments.

--- js/carousel.js
+++ js/carousel.js
@@ -2,30 +2,38 @@ $(document).ready(function() {
   $("#steps-menu a").click(function(event) {
     // Replaces main content
     event.preventDefault();
+
+    var step = $(this).attr("href");
+    var stepWrap = $(this).closest("#steps");
+    var stepMenu = $(this).closest("#steps-menu");
+    var stepClass = step.charAt(6);
+
     $(this).parent().addClass("is-active");
     $(this).parent().siblings().removeClass("is-active");
-    var step = $(this).attr("href");
-    $(".step-content").not(step).css("display", "none");
-    $(step).fadeToggle();
+    $(stepWrap).find(".step-content").not(step).css("display", "none");
+    $(stepWrap).find(step).fadeToggle();
 
     // Rotates the wheel
-    $("#steps-menu").removeClass();
-    var stepClass = step.charAt(6);
-    $("#steps-menu").addClass("step-" + stepClass);
+    $(stepMenu).removeClass();
+    $(stepMenu).addClass("step-" + stepClass);
   });
 
   $(".read-more").click(function(event) {
     // Replaces main content
     event.preventDefault();
+
     var step = $(this).attr("href");
-    $(".step-content").not(step).css("display", "none");
-    $(step).fadeToggle();
+    var stepWrap = $(this).closest("#steps");
+    var stepMenu = $(stepWrap).find("#steps-menu");
+    var stepClass = step.charAt(6);
+
+    $(stepWrap).find(".step-content").not(step).css("display", "none");
+    $(stepWrap).find(step).fadeToggle();
 
     // Set and rotates the wheel
-    var stepClass = step.charAt(6);
-    $("#link"+stepClass).parent().addClass("is-active");
-    $("#link"+stepClass).parent().siblings().removeClass("is-active");
-    $("#steps-menu").removeClass();
-    $("#steps-menu").addClass("step-" + stepClass);
+    $(stepMenu).find("li:nth-child("+stepClass+")").addClass("is-active");
+    $(stepMenu).find("li:nth-child("+stepClass+")").siblings().removeClass("is-active");
+    $(stepMenu).removeClass();
+    $(stepMenu).addClass("step-" + stepClass);
   });
 });
