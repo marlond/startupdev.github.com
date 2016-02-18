c15564502664941caf46979cf928ed817e887c80
Kalyne Ramon <kalynek@gmail.com>
2016-02-18 10:16:14 -0200

    Add wufoo custom form

--- js/site2016.js
+++ js/site2016.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+
+  // Slider of testimonials
+  //---------------------------------------------------------------
   $('.bxslider').bxSlider({
     mode: 'fade',
     pager: false,
@@ -9,9 +12,107 @@ $(document).ready(function(){
     adaptiveHeight: true,
     responsive: true
   });
-});
 
-$(function() {
+  // Form Wufoo validated with jquery
+  //---------------------------------------------------------------
+  $(".js-register-form").validate({
+
+    //debug: true,
+
+    // Define as regras
+    rules:{
+
+      Field43:{
+        //name
+        required: true, minlength: 2
+      },
+
+      Field44:{
+        // sobrenome
+        required: true, minlength: 2
+      },
+
+      Field41:{
+        // projeto e empresa
+        required: true, minlength: 2
+      },
+
+      Field3:{
+        // email
+        required: true, email: true
+      },
+
+      Field16:{
+        // telefone
+        required: true, number: true
+      },
+
+      Field52:{
+        // skype
+        required: true, minlength: 2
+      },
+
+      Field48:{
+        // descriçao do projeto
+        required: true, minlength: 2
+      }
+
+    },
+
+    // Define as mensagens de erro para cada regra
+    messages:{
+
+      Field43:{
+        //name
+        required: "Por favor, preencha com seu nome."
+      },
+
+      Field44:{
+        // sobrenome
+        required: "Por favor, preencha com seu sobrenome."
+      },
+
+      Field41:{
+        // projeto e empresa
+        required: "Por favor, preencha com o nome do projeto."
+      },
+
+      Field3:{
+        // email
+        required: "Por favor, preencha com um e-mail válido."
+      },
+
+      Field16:{
+        // telefone
+        required: "Por favor, preencha com um telefone válido."
+      },
+
+      Field52:{
+        // skype
+        required: "Por favor, preencha com seu nome."
+      },
+
+      Field48:{
+        // descriçao do projeto
+        required: "Por favor, descreva brevemente seu projeto."
+      }
+
+    },
+
+    // submit form
+    submitHandler: function(form) {
+      form.submit();
+    }
+
+
+  });
+
+  jQuery.extend(jQuery.validator.messages, {
+    maxlength: jQuery.validator.format("Please enter no more than {0} characters."),
+    minlength: jQuery.validator.format("Este campo deve conter, no mínimo, {0} caracteres"),
+    email: "Por favor, preencha com um e-mail válido.",
+    number: "Por favor, preencha com um telefone válido.",
+  });
 
   // Hamburger mobile menu
   //---------------------------------------------------------------
@@ -29,4 +130,17 @@ $(function() {
     $btnMenu.toggleClass('toggle--close');
   });
 
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
